ef0e2345d26a8f5fdeaf1ba6a62bc8b10b333c7d
Neal Hansen <neal.hansen@onapp.com>
2019-01-31 13:11:19 -0700

    Change CP IP to be defined with an argument rather than a variable inside

--- fix_dhcp.sh
+++ fix_dhcp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CPIP=<MAIN CONTROL PANEL IP>
+CPIP=$1
 
 if [[ /onapp/configuration/dhcp/dhcpd.conf -nt /etc/dhcp/dhcpd.conf ]] ; then
   sed -e "s/next-server $CPIP/next-server relay.local/g" /onapp/configuration/dhcp/dhcpd.conf > /etc/dhcp/dhcpd.conf
