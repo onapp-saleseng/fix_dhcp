0bc5af0eb8a8c122fc53ffb788031f79aeeb7089
onapp-saleseng <46685498+onapp-saleseng@users.noreply.github.com>
2019-02-28 18:57:13 -0700

    Update fix_dhcp.sh
    add domain name change
    touch dhcp.conf

--- fix_dhcp.sh
+++ fix_dhcp.sh
@@ -3,6 +3,7 @@
 CPIP=$1
 
 if [[ /onapp/configuration/dhcp/dhcpd.conf -nt /etc/dhcp/dhcpd.conf ]] ; then
-  sed -e "s/next-server $CPIP/next-server relay.local/g" /onapp/configuration/dhcp/dhcpd.conf > /etc/dhcp/dhcpd.conf
+  sed -e "s/next-server $CPIP;/next-server relay.local;/;s/domain-name-servers $CPIP;/domain-name-servers relay.local;/" /onapp/configuration/dhcp/dhcpd.conf > /etc/dhcp/dhcpd.conf
+  touch /etc/dhcp/dhcpd.conf
   systemctl restart dhcpd
 fi
